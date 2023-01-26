6e0b93e5477f2d415d053aa5569054bd7617e9b8
jinshendan <jinshendan@gmail.com>
2023-01-26 22:12:59 +0100

    Add Path Sum C++ Solution

---
+++ Problems/04-Path-Sum/Path-Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+        if (root->left == nullptr and root->right == nullptr) return root->val == targetSum;
+        return hasPathSum(root->left, targetSum-root->val) or hasPathSum(root->right, targetSum-root->val);
+    }
+};
