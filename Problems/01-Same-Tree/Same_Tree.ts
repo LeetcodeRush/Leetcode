c57f624f9d8be131f53c2d72c9a1ca325c6b9ae3
JeremyXXXuuu <jeremyxu1234@gmail.com>
2023-01-26 14:05:21 +0100

    Add Same Tree TS Solution

---
+++ Problems/01-Same-Tree/Same_Tree.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+
+    if(!p && q) return false;
+    else if(p && !q) return false;
+    else if(!p && !q) return true;
+    else if(p.val !== q.val) return false;
+    
+    let left = isSameTree(p.left, q.left);
+    let right = isSameTree(p.right, q.right);
+    return left && right;
+
+};
