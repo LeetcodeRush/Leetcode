9f14d415cf8989dd3b60e8342d60f8fd1780ee2b
qinyii <qyxkay@gmail.com>
2023-02-02 21:41:40 +0100

    Add python solution

---
+++ Problems/09-Longest-Subsequence-With-Limited-Sum/Longest_Subsequence_With_Limite.py
@@ -0,0 +1,23 @@
+import bisect
+
+class Solution:
+    def findTheDistanceValue1(self, arr1, arr2, d):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :type d: int
+        :rtype: int
+        """
+        return sum(all(abs(x - y) > d for y in arr2) for x in arr1)
+
+        
+    def findTheDistanceValue(self, arr1, arr2, d):
+        arr2.sort()
+        res = 0
+        for n in arr1:
+            i = bisect.bisect_left(arr2, n)
+            if (i >= len(arr2) or arr2[i]-n > d) and (i == 0 or n - arr2[i-1] > d):
+                res += 1
+        return res
+
+
